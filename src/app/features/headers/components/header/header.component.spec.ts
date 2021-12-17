46c987a34d8efc8f8d2662e9b36b8b93b242edc1
BrenoES <breno.esilva@outlook.com>
2021-12-17 12:18:58 -0300

    feat(header): add button login and logout

--- src/app/features/headers/components/header/header.component.spec.ts
+++ src/app/features/headers/components/header/header.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { HeadersModule } from '@features/headers/headers.module';
 
 import { HeaderComponent } from './header.component';
@@ -9,7 +10,7 @@ describe('HeaderComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HeadersModule],
+      imports: [HeadersModule, RouterTestingModule],
     }).compileComponents();
   });
 
--- src/app/features/headers/components/header/header.component.ts
+++ src/app/features/headers/components/header/header.component.ts
@@ -1,17 +1,49 @@
+import { AfterViewInit, ViewChild } from '@angular/core';
+import { ElementRef } from '@angular/core';
 import { Component, EventEmitter, Output } from '@angular/core';
-import { faSearch, faUserLock } from '@fortawesome/free-solid-svg-icons';
+import { AuthService } from '@core/services/auth.service';
+import {
+  faBookmark,
+  faSearch,
+  faSignInAlt,
+  faSignOutAlt,
+} from '@fortawesome/free-solid-svg-icons';
+import { fromEvent, filter, debounceTime, distinctUntilChanged } from 'rxjs';
+import { HasEventTargetAddRemove } from 'rxjs/internal/observable/fromEvent';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements AfterViewInit {
   @Output() search = new EventEmitter();
-  faUserLock = faUserLock;
-  faSearch = faSearch;
 
-  searchChange(value: string) {
-    this.search.emit(value);
+  @ViewChild('input', { static: true }) input!: ElementRef;
+
+  faSignInAlt = faSignInAlt;
+  faSignOutAlt = faSignOutAlt;
+  faSearch = faSearch;
+  faBookmark = faBookmark;
+
+  constructor(public auth: AuthService) {}
+
+  ngAfterViewInit() {
+    fromEvent(
+      this.input.nativeElement as HasEventTargetAddRemove<HTMLInputElement>,
+      'keyup'
+    )
+      .pipe(filter(Boolean), debounceTime(350), distinctUntilChanged())
+      .subscribe(() =>
+        this.search.emit((<HTMLInputElement>this.input.nativeElement).value)
+      );
+  }
+
+  async logout() {
+    await this.auth.SignOut();
+  }
+
+  get isLoggedIn() {
+    return this.auth.isLoggedIn;
   }
 }
